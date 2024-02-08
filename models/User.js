8164c96bc58b7209bb7b2fde71bab1cc6d2da725
salidamaharjan <salidamaharjan@outlook.com>
2024-02-08 17:33:26 -0600

    create User Model

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8, 22],
+      },
+    },
+  },
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    timestamps: false,
+  }
+);
--- server.js
+++ server.js
@@ -1,1 +1,4 @@
+const sequelize = require('./config/connection');
 
+sequelize.sync({force: true});
+console.log('synchronized successfully');
