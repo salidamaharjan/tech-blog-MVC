4f890c121b24c082863d032e6bb0a2a2d6aba36f
salidamaharjan <salidamaharjan@outlook.com>
2024-02-08 17:08:22 -0600

    set up connection with database'

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+const {DB_NAME, DB_PASSWORD, DB_USER} = process.env
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: "127.0.0.1",
+    dialect: 'mysql'
+});
+
+module.exports = sequelize;
