83b7a2d63f48004dfd3809783b2fa6925c8b63a1
salidamaharjan <salidamaharjan@outlook.com>
2024-02-09 22:28:28 -0600

    set up login route

--- controller/index.js
+++ controller/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const homeRoute = require('./home-routes');
+const loginRoute = require('./login-routes');
 
 router.use(homeRoute);
+router.use(loginRoute);
 
 module.exports = router;
