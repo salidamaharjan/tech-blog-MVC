a51d371c7cbe72541a751fc8243b9c4256cf56fb
salidamaharjan <salidamaharjan@outlook.com>
2024-02-09 18:31:43 -0600

    set up handlebars and use middleware

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.store);
-const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const sequelize = require('./config/connection');
+const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,12 +20,20 @@ const ses = {
     })
 }; 
 
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session(ses));
+app.use(express.json());
+
 runMVC();
 
 async function runMVC(){
     await sequelize.sync({force: true});
     console.log('synchronized successfully');
     app.listen(PORT, () => {
-        console.log(`Listening at ${PORT}`);
+        console.log(`Listening at http://localhost:${PORT}`);
     });
 }
